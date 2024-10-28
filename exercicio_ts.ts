6f10dc975123788ccb4e471286e6801e8dd4f00d
Bruno <155686844+Bruno-dev-n@users.noreply.github.com>
2024-10-28 10:41:01 -0300

    Update exercicio_ts.ts

--- exercicio_ts.ts
+++ exercicio_ts.ts
@@ -1,45 +1,12 @@
-class Multiplicacao {
-    investimento: number;
-    juros: number;
-    total: number;
-
-    constructor(investimento: number, juros: number) {
-        this.investimento = investimento;
-        this.juros = juros;
-        this.total = 0;
-    }
-
-
-    protected calcularInvestimento(): number {
-        return this.investimento * (this.juros / 100);
-    }
-
-    public getTotal(): number {
-        this.total = this.investimento + this.calcularInvestimento();
-        return this.total;
-    }
-    
+function multiplicar(a: number, b: number): number {
+    return a * b;
 }
-class saudacao{
-    private diz: string;
-    private nome: string;
-    private Saudando: string = '';
 
-    constructor( diz: string = "ola", nome: string = "Bruno"){
-        this.diz=diz;
-        this.nome=nome;
-        this.Saudando=this.Saudando;
-    }
-    protected concatSaudacao(): string{
-        this.Saudando = this.diz + " " +this.nome;
-        return this.Saudando;
-    }
 
-public getSaudando():string{
-    return this.concatSaudacao();
+console.log(multiplicar(4, 5));
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
 }
-}
-const Asaudacao = new saudacao()
-console.log(Asaudacao.getSaudando());
-const minhaMultiplicacao = new Multiplicacao(215, 2.5);
-console.log(`Investimento total com juros: ${minhaMultiplicacao.getTotal()}`);
+
+console.log(saudacao("Bruno"));
